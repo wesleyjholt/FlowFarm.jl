9d732426f84e88b117c569939320cd8d968ca51f
Wesley Holt <wesleyjholt@gmail.com>
2021-02-16 12:32:57 -0700

    add wakecount function

--- src/utilities.jl
+++ src/utilities.jl
@@ -431,3 +431,64 @@ function coordDist(x1, y1, x2, y2)
     yDiff = y1 - y2
     return sqrt(xDiff^2 + yDiff^2)
 end
+
+# function calculate_wakecount(turbine_x, turbine_y, rotor_diamter, winddirection)
+#     """
+#     Finds the number of turbines waking each turbine for the given
+#     wind direction. Waked directions are determined using the formula
+#     in Figure A.1 in Annex A of the IEC 61400-12-1:2017 standard.
+#     Args:
+#         turbineX (numpy.ndarray): x-coordinates for turbines in the farm
+#         turbineY (numpy.ndarray): y-coordinates for turbines in the farm
+#         wd (float): Wind direction (in meteorological coordinate system) for determining waked turbines.
+#     Returns:
+#         list(int): N
+#     """
+#     nturbines = length(turbine_x)
+#     for i = 1:length(turbine_x)
+#         other_turbines = 
+
+# function plotFarm(turbine_x, turbine_y, boundary_vertices::Array{Float64}; windfield=false, wakecount=false)
+
+#     """
+#     Author: Jared J. Thomas
+#     Date:   May 29, 2020
+
+#     Test based on:
+#     [1] An Aero-acoustic Noise Distribution Prediction Methodology for Offshore Wind Farms
+#     by Jiufa Cao, Weijun Zhu, Xinbo Wu, Tongguang Wang, and Haoran Xu
+#     """
+
+# using FlowFarm; const ff = FlowFarm
+# using DelimitedFiles
+# using PyPlot
+
+# # load problem set up
+# include("./model_sets/model_set_6.jl")
+
+# # calculate wind turbine velocities and corresponding aerodynamic operational states
+# turbine_inflow_velocities, turbine_ct, turbine_ai, turbine_local_ti = ff.turbine_velocities_one_direction(turbine_x, turbine_y, turbine_z, rotor_diameter, hub_height, turbine_yaw,
+# sorted_turbine_index, ct_models, rotor_sample_points_y, rotor_sample_points_z, windresource,
+# model_set, wind_farm_state_id=1, velocity_only=false)
+
+# # calculate the power production of each wind turbine
+# # ff.turbine_powers_one_direction(generator_efficiency, cut_in_speed, cut_out_speed, rated_speed, rated_power, rotor_diameter, turbine_inflow_velcities, air_density, power_models)
+
+# if windfield
+#     # set up the point locations to be used in generating the plot
+#     stepsize = 5
+#     xrange = minimum(turbine_x)-2*rotor_diameter:stepsize:maximum(turbine_x)+2*rotor_diameter
+#     yrange = minimum(turbine_y)-2*rotor_diameter:stepsize:maximum(turbine_y)+2*rotor_diameter
+#     zrange = minimum(hub_height):stepsize:maximum(hub_height)
+
+#     # calculate the wind speed at each point in the flow field 
+#     velh = ff.calculate_flow_field(xrange, yrange, zrange,
+#     model_set, turbine_x, turbine_y, turbine_z, turbine_yaw, turbine_ct, turbine_ai,
+#     rotor_diameter, hub_height, turbine_local_ti, sorted_turbine_index, turbine_inflow_velocities,
+#     windresource; wind_farm_state_id=1)
+
+#     # visualize the resulting data
+#     matplotlib.use("tkagg")
+#     plt.contourf(xrange, yrange, velh, cmap="Blues_r")
+#     plt.show()
+# end
